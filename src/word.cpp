177f4df700afd4b5f8e03837d139faf77cf563f2
MateuszKwiecien <mateo_kw@outlook.com>
2024-10-05 15:37:50 +0200

    Added letter display update

--- src/word.cpp
+++ src/word.cpp
@@ -2,18 +2,18 @@
 
 Word::Word(const std::string& answer, const sf::Font& font, unsigned charsize, sf::Color wordcolor)
 {
-	std::string temp;
-	temp.assign(answer.length(), '_');
+	displayed.clear();
+	for(const char& letter : answer) {
+		displayed += '_';
+	}
 
 	setFont(font);
-	setString(temp);
+	setString(displayed);
 	setCharacterSize(charsize);
 	setFillColor(wordcolor);
 	setLetterSpacing(5.0f);
 	this->answer = answer;
 	sf::FloatRect textBounds = getLocalBounds();
-
-
 }
 
 
@@ -25,7 +25,19 @@ void Word::handleInputs(const sf::Event& event)
 	{
 		// Cast the key to the corresponding char ('A' has value 0 in sf::Keyboard::A)
 		char letter = static_cast<char>(event.key.code + 'A');
-		std::cout << "Letter pressed: " << letter << std::endl;
+		this->showLetter(letter);
 	}
 };
+
+void Word::showLetter(const char& letter) {
+	size_t pos = answer.find(letter);
+	while (pos != sf::String::InvalidPos) {
+		displayed[pos] = answer[pos];
+
+		pos = answer.find(letter, pos + 1);
+	}
+
+	setString(displayed);
+}
+
 
--- src/word.hpp
+++ src/word.hpp
@@ -2,36 +2,18 @@
 
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Window.hpp>
-#include <string>
-#include <iostream>
 
 class Word : public sf::Text
 {
 private:
 	sf::String answer;
+	sf::String displayed;
 	
 public:
 	Word(const std::string& answer, const sf::Font& font, unsigned charsize, sf::Color wordcolor);
 	
 	
 	void handleInputs(const sf::Event& event);
-	
-};
 
-//sf::Font font;
-//if (!font.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf")) {
-//    // Obsługa błędu ładowania czcionki
-//    return -1;
-//}
-//
-//sf::Text text;
-//text.setFont(font);
-//text.setString(word);
-//text.setCharacterSize(50);
-//
-//text.setFillColor(sf::Color::White);
-//
-//sf::FloatRect textBounds = text.getLocalBounds();
-//
-//text.setOrigin(textBounds.width / 2, textBounds.height / 2);
-//text.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+	void showLetter(const char &letter);
+};
